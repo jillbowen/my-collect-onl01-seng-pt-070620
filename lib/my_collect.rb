2a31a12379d9316a4aba8c29579927de26c55956
Jill Short <jillian.b.short@gmail.com>
2020-07-16 00:18:39 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,3 +8,12 @@ def my_collect(empty_array)
   empty_array
 end
 
+# def hello(array)
+#   i = 0
+#   collection = []
+#   while i < array.length
+#     collection << yield(array[i])
+#     i += 1
+#   end
+#   collection
+# end
